746d9f4abc406724c6a719016128236ea022a9cd
xiaoxingzeng <hndxzxx@163.com>
2016-09-30 16:50:57 +0800

    kp_model

---
+++ ONet/ONet_trainval.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+LOG=log/train-`date +%Y-%m-%d-%H-%M-%S`.log
+
+./build/tools/caffe train --solver=/home/zqzeng/kp_model_caffe/trainV1/creatdata.ONet/model/solver.prototxt --gpu 0,1,2,3 2>&1 | tee $LOG
+
